fd0b73a7659db00cd49197c9d2a89ff4cdad768e
ChanTolaRen <renchantola168@gmail.com>
2025-01-28 20:57:34 +0700

    Update

--- W4/Doc/Day 5/Server/Route/TweetRoute.js
+++ W4/Doc/Day 5/Server/Route/TweetRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const Route = express.Router();
-
 const { creataTweet, showTweet } = require("../Controller/TweetController");
 Route.get("/tweets", showTweet);
 Route.post("/tweets", creataTweet);
